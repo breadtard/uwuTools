db541d0f3e8674d596c0faa5670a8c2a289c063c
MrVeink <joona.holkko@tuta.io>
2021-05-08 23:44:17 +0300

    Fixed uwuEdit. Didn't allow typing at first.

--- uwuEdit/main.c
+++ uwuEdit/main.c
@@ -1,6 +1,8 @@
 #include <unistd.h>
 #include <termios.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
 
 struct termios orig_termios;
 
@@ -12,7 +14,7 @@ void enableRawMode() {
 	tcgetattr(STDIN_FILENO, &orig_termios);
 	atexit(disableRawMode);
 	struct termios raw = orig_termios;
-	raw.c_lflag &= ~(ECHO);
+	raw.c_lflag &= ~(ECHO | ICANON);
 
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -20,6 +22,12 @@ void enableRawMode() {
 int main() {
 	enableRawMode();
 	char c;
-	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+		if (iscntrl(c)) {
+			printf("%d\n", c);
+		} else {
+			printf("%d ('%c')\n", c, c);
+		}
+	}
 	return 0;
 }
